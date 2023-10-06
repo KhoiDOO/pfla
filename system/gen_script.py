5c2aa06dd15ac7b97c7ff8dfd0b17aa108b05180
KhoiDOO <khoido8899@gmail.com>
2023-10-06 18:15:04 +0700

    update script

--- system/gen_script.py
+++ system/gen_script.py
@@ -2,7 +2,7 @@ import os
 import json
 
 if __name__ == "__main__":
-    run_file = os.getcwd() + "/run.sh"
+    run_file = os.getcwd() + "/runv2.sh"
     sweep_file = os.getcwd() + "/sweep.json"
     
     sweep_data = json.load(open(sweep_file, mode='r'))
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     algo = sweep_data["-algo"] 
     ds = sweep_data["-data"]
     nc = sweep_data["-nc"]
-    m = sweep_data["-m"]
     go = sweep_data["-go"]
     
     cmd_lst = []
@@ -18,11 +17,10 @@ if __name__ == "__main__":
     for _algo in algo:
         for _ds in ds:
             for _nc in nc:
-                for _m in m:
-                    for _go in go:
-                        cmd_lst.append(
-                            f"python -u main.py -lbs 16 -nc {_nc} -jr 1 -nb 10 -data {_ds['name']} -nb {_ds['#cls']} -m {_m} -algo {_algo} -gr 100 -did 0 -bt 0.001 -go {_go} -fceal\n"
-                        )
+                for _go in go:
+                    cmd_lst.append(
+                        f"python -u main.py -lbs 16 -nc {_nc} -jr 1 -nb 10 -data {_ds['name']} -nb {_ds['#cls']} -m {_ds['-m']} -algo {_algo} -gr 100 -did 0 -bt 0.001 -go {_go} -fceal\n"
+                    )
                         
     with open(run_file, mode='w') as file:
         file.writelines(
