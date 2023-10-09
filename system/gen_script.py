eb755c6f3015b9da22bdcf78d20e8c2aa576f676
KhoiDOO <khoido8899@gmail.com>
2023-10-09 21:20:56 +0700

    update gen_script

--- system/gen_script.py
+++ system/gen_script.py
@@ -2,7 +2,7 @@ import os
 import json
 
 if __name__ == "__main__":
-    run_file = os.getcwd() + "/runv2.sh"
+    # run_file = os.getcwd() + "/runv2.sh"
     sweep_file = os.getcwd() + "/sweep.json"
     
     sweep_data = json.load(open(sweep_file, mode='r'))
@@ -12,18 +12,20 @@ if __name__ == "__main__":
     nc = sweep_data["-nc"]
     go = sweep_data["-go"]
     
-    cmd_lst = []
-    
     for _algo in algo:
+        cmd_lst = []
+        run_file = os.getcwd() + f"/{_algo}.sh"
         for _ds in ds:
             for _nc in nc:
-                for _go in go:
+                if _nc["nc"] == 20 and _ds["name"] in ["Cifar10", "Cifar100", "mnist"]:
+                    continue
+                else:
                     cmd_lst.append(
-                        f"python -u main.py -lbs 16 -nc {_nc} -jr 1 -nb 10 -data {_ds['name']} -nb {_ds['#cls']} -m {_ds['-m']} -algo {_algo} -gr 100 -did 0 -bt 0.001 -go {_go} -fceal\n"
+                        f"python -u main.py -lbs 16 -nc {_nc['nc']} -jr 1 -nb 10 -data {_ds['name']} -nb {_ds['#cls']} -m {_ds['-m']} -algo {_algo} -gr {_nc['gr']} -did 0 -bt 0.001 -go train -fceal\n"
                     )
                         
-    with open(run_file, mode='w') as file:
-        file.writelines(
-            cmd_lst
-        )
-        file.close()
+        with open(run_file, mode='w') as file:
+            file.writelines(
+                cmd_lst
+            )
+            file.close()
